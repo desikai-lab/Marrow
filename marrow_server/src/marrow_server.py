b4d6a043265eb9326bc10446eebc2dfe2ccf6295
Pavelko Denis <desikdes@yahoo.com>
2026-06-25 00:15:42 +0300

    fixed ruff

--- marrow_server/src/marrow_server.py
+++ marrow_server/src/marrow_server.py
@@ -22,4 +22,10 @@ if __name__ == "__main__":
 
     # Check config before running
     check_startup_config()
-    uvicorn.run(app, host=os.getenv("HOST", "0.0.0.0"), port=int(os.getenv("PORT", "8000")), proxy_headers=True, forwarded_allow_ips="*")
+    uvicorn.run(
+        app,
+        host=os.getenv("HOST", "0.0.0.0"),
+        port=int(os.getenv("PORT", "8000")),
+        proxy_headers=True,
+        forwarded_allow_ips="*",
+    )
